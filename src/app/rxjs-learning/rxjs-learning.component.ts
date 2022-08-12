d93c048e904275814bb8c1de24ef91d7a9511131
ArturOnEuvic <artur.koziol@euvic.pl>
2022-08-12 09:06:41 +0200

    Rxjs-learning

--- src/app/rxjs-learning/rxjs-learning.component.ts
+++ src/app/rxjs-learning/rxjs-learning.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Observable, of} from 'rxjs';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { tap, pipe } from 'rxjs';
+import { tap} from 'rxjs';
 
 
 @Component({
