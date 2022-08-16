9d6762f6cd8690843faa8d86db69e1cc8228f2d8
ArturOnEuvic <artur.koziol@euvic.pl>
2022-08-16 12:16:10 +0200

    setInterval_task

--- src/app/rxjs-learning/rxjs-learning.component.ts
+++ src/app/rxjs-learning/rxjs-learning.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {Observable, of} from 'rxjs';
+import {interval, Observable, of, tap, range, min, max } from 'rxjs';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { tap} from 'rxjs';
+import {BehaviorSubject} from 'rxjs';
+
+
+
 
 
 @Component({
@@ -10,6 +13,26 @@ import { tap} from 'rxjs';
   styleUrls: ['./rxjs-learning.component.css']
 })
 export class RxjsLearningComponent implements OnInit {
+  title = 'toolsets'
+  data: number = 0;
+
+
+
+  
+setInterval(): any{
+const data = range(1,10);
+this.data = Math.floor(min)
+this.data = Math.floor(max)
+this.data = Math.floor(Math.random() * max - min + 1) + min;
+data.subscribe((d) => {
+  console.log(d);
+  this.data = d;
+  });
+}
+
+
+
+
 
 
 tablicaTestowa?:Observable< number[] |null>
@@ -17,14 +40,16 @@ tablicaTestowa?:Observable< number[] |null>
   ngOnInit(): any {
     this.tablicaTestowa = of([1,2,3,4,5]).pipe(tap(console.log))
   }
-
   constructor(private snackBar: MatSnackBar) {}
-
   openSnackBar(message: number[] |null){
     if(message){
     this.snackBar.open(message.join(','));
     }
   }
+
+    
+    
+  
 }
 
 
